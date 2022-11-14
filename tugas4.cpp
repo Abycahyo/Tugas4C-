bf85f79c1a0b5a23d46cabffef91d6530495480f
aby <117300665+Abycahyo@users.noreply.github.com>
2022-11-14 20:16:03 +0700

    Create tugas4.cpp

---
+++ tugas4.cpp
@@ -0,0 +1,34 @@
+
+
+#include
+
+using namespace std;
+
+int main()
+{
+cout << "## Tugas 4 Program C++ Menentukan Ganjil-Genap ##" << endl;
+cout << "====================================================" << endl;
+
+cout << "nama : Windha Aby Cahyo"<< endl;
+cout << "nim : 5520122010"<< endl;
+cout << endl;
+cout << "====================================================" << endl;
+int angkabilangan;
+cout<<"Menentukan Genap dan Ganjil"<<endl;
+cout<<"Silahkan Masukan Angka : "<<endl;
+cin>>angkabilangan;
+cout<<endl;
+
+cout << "=========================MAKA!======================" << endl;
+
+if (angkabilangan%2==0){
+cout << "Angka "<<angkabilangan<<" Adalah bilangan genap"<<endl;
+}else {
+cout << "Angka "<<angkabilangan<<" Adalah bilangan ganjil"<<endl;
+}
+
+cout << "====================================================" << endl;
+cout << endl;
+return 0;
+
+}
